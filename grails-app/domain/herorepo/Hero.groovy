7fb63da1a228696cd880dcd91b0fe8a0bbe506e2
Paul Szyller <paulszyller@gmail.com>
2018-07-22 18:14:22 -0500

    added Hero class properties and constraints

--- grails-app/domain/herorepo/Hero.groovy
+++ grails-app/domain/herorepo/Hero.groovy
@@ -1,7 +1,19 @@
 package herorepo
 
 class Hero {
+    String name
+    String title
+    String race
+    String gender
+    int age
+    String heroClass
 
     static constraints = {
+        name unique: true, blank: false, minSize: 3, maxSize: 32, matches: "[a-zA-Z]+"
+        title nullable: true, minSize: 3, maxSize: 32
+        race inList: ["Dwarf", "Elf", "Halfling", "Human", "Orc"]
+        gender inList: ["Female", "Male"]
+        age min: 1
+        heroClass inList: ["Barbarian", "Bard", "Druid", "Monk", "Paladin", "Ranger", "Sorcerer", "Warlock"]
     }
 }
